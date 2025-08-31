ba3cd8515c7c2e8877e18639516dc94b391dce2f
qwreey <me@qwreey.moe>
2025-08-31 15:18:54 +0900

    sleep removed

--- root/etc/tuned/profiles/qwreey-battery-powersave/script.sh
+++ root/etc/tuned/profiles/qwreey-battery-powersave/script.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
 echo low | tee /sys/class/drm/card[0-9]/device/power_dpm_force_performance_level
 echo powersupersave | tee /sys/module/pcie_aspm/parameters/policy
-sleep 2
 ryzenadj --power-saving
--- root/etc/tuned/profiles/qwreey-plugged-performance/script.sh
+++ root/etc/tuned/profiles/qwreey-plugged-performance/script.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
 echo auto | tee /sys/class/drm/card[0-9]/device/power_dpm_force_performance_level
 echo performance | tee /sys/module/pcie_aspm/parameters/policy
-sleep 2
 ryzenadj --max-performance
