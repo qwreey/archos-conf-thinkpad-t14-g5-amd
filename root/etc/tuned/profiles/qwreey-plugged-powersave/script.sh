04487c3feae8d9a576b14c70abea10426b9e5eeb
qwreey <me@qwreey.moe>
2025-09-03 15:08:40 +0900

    Fix crash (maybe)

--- root/etc/tuned/profiles/qwreey-plugged-powersave/script.sh
+++ root/etc/tuned/profiles/qwreey-plugged-powersave/script.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 echo low | tee /sys/class/drm/card[0-9]/device/power_dpm_force_performance_level
 echo powersupersave | tee /sys/module/pcie_aspm/parameters/policy
+sleep 0.3
 ryzenadj --power-saving
